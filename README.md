aa3b26baa6632697601c88255c8de86131da8770
Ankit Barai <ankitbarai2023@gmail.com>
2026-01-22 23:51:08 +0530

    initial setup complete

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1. add getx package to the project
 
 2.materialApp to GetMaterialApp
+
+to set height ==> Get.height*.5 (50% of the screen)
+to set width ==> Get.width*.8 (80% of the screen)
+
+Get.snackbar("title", "message"); // you can use various properties to make this snackbar beautiful, you can call the snackbar either
+from top or bottoom
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,6 +18,30 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.tealAccent,
       ),
-    );
+
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Get.snackbar(
+              "Learn Flutter",
+              "I am Learning Flutter with GetX Package",
+              snackPosition: SnackPosition.TOP,
+              backgroundColor: Colors.blue[300],
+              colorText: Colors.white,
+              borderRadius: 10,
+              margin: EdgeInsets.all(15),
+              duration: Duration(seconds: 3),
+              icon: Icon(Icons.info, color: Colors.white,),
+            );
+          },
+          child: Container(
+            height: Get.height * 0.4,
+            width: Get.width * 0.8,
+            color: Colors.amber,
+          ),
+        ),
+        ),
+      );
   }
 }
+
